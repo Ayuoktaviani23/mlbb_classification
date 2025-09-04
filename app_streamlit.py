695c2789d62d1b4844365fb70f33d7a45aac9b23
Ayuoktaviani23 <Oktavianisja@gmail.com>
2025-09-04 07:52:27 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -10,10 +10,10 @@ st.markdown("Aplikasi Machine Learning Sederhana")
 kill = st.slider("Jumlah Kill",0,20)
 asisst = st.slider("Jumlah Assist",0,20)
 death = st.slider("Jumlah Death",0,20)
-turret = st.slider("Jumlah Turret",0,20)
+turet = st.slider("Jumlah Turret",0,20)
 
 if st.button("Prediksi"):
-	data_baru = pd.DataFrame([[kill,asisst,death,turret]], columns=["kill","asisst","death","turret"])
+	data_baru = pd.DataFrame([[kill,asisst,death,turet]], columns=["kill","asisst","death","turret"])
 	hasil = model.predict(data_baru)[0]
 	if hasil == "penyerang":
 		st.success(f"Hasil Prediksinya : {hasil}")
@@ -24,3 +24,4 @@ if st.button("Prediksi"):
 
 
 st.caption("Dibuat dengan :skull: dan :fire: oleh Ayu Oktaviani")
+
